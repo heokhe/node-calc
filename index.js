f17f3527238f2a518d6ed40ed2ccc6fe35034f71
Hosein Khansari <hrk.x11@gmail.com>
2019-11-14 16:43:33 +0330

    Fix crash when an empty expression is received

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ console.log('Welcome to Node.js calculator!');
 const rl = readline.createInterface(process.stdin, process.stdout);
 function askForAnExpression() {
   rl.question(blueBright('> '), expr => {
-    try {
-      console.log(evalExpression(expr).toString());
-    } catch (e) {
-      console.log(redBright(e.message));
+    if (expr.trim()) {
+      try {
+        console.log(evalExpression(expr).toString());
+      } catch (e) {
+        console.log(redBright(e.message));
+      }
     }
     askForAnExpression();
   });
