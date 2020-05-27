04255270c17a0d939d4e075f63346211f5a98060
Hosein Khansari <hrk.x11@gmail.com>
2020-05-27 18:17:05 +0430

    convert to library

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-#!/usr/bin/node
-const readline = require('readline');
-const { redBright, blueBright } = require('chalk');
 const evalExpression = require('./eval');
-
-console.log('Welcome to Node.js calculator!');
-const rl = readline.createInterface(process.stdin, process.stdout);
-rl.addListener('close', () => console.log('\nBye Bye!'));
-function askForAnExpression() {
-  rl.question(blueBright('> '), expr => {
-    if (expr.trim()) {
-      try {
-        console.log(evalExpression(expr).toString());
-      } catch (e) {
-        console.log(redBright(e.message));
-      }
-    }
-    askForAnExpression();
-  });
-}
-askForAnExpression();
+module.exports = evalExpression;
