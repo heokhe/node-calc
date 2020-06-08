194140c670d7c040df51572dfe79b9cb81f66b61
Hosein Khansari <hrk.x11@gmail.com>
2020-06-08 11:51:42 +0430

    use tap.throws in errors.test.js

--- tests/errors.test.js
+++ tests/errors.test.js
@@ -1,28 +1,17 @@
 const tap = require('tap');
 const { tokenize, evalTokens } = require('../lib');
 
-const expectError = (testName, fn) => {
-  tap.test(testName, t => {
-    try {
-      fn();
-      t.fail();
-    } catch (_) {
-      t.end();
-    }
-  });
-};
-
-expectError('throws an error when no expression is there', () => {
+tap.throws(() => {
   tokenize('');
   tokenize(' ');
 });
-expectError('throws an error when no token is there', () => evalTokens([]));
-expectError('throws an error when there\'s an unexpected whitespace', () => tokenize('2 3'));
-expectError('throws when an unexpected character comes in', () => tokenize('!'));
-expectError('throws when an operator needs a left-hand side expression', () => tokenize('/ 5'));
-expectError('throws when an operator expected a right-hand side expression', () => tokenize('2 * '));
-expectError('throws when there\'s an unknown function', () => evalTokens(tokenize('foo(4)')));
-expectError('throws when a function is written with no braces', () => evalTokens(tokenize('sin45')));
-expectError('throws when a function is written with arguments', () => evalTokens(tokenize('sin()')));
-expectError('throws when only a function name is present', () => evalTokens(tokenize('2 + sin')));
-expectError('throws when parenthesis are not balanced', () => evalTokens(tokenize('(2+2')));
+tap.throws(() => evalTokens([]));
+tap.throws(() => tokenize('2 3'));
+tap.throws(() => tokenize('!'));
+tap.throws(() => tokenize('/ 5'));
+tap.throws(() => tokenize('2 * '));
+tap.throws(() => evalTokens(tokenize('foo(4)')));
+tap.throws(() => evalTokens(tokenize('sin45')));
+tap.throws(() => evalTokens(tokenize('sin()')));
+tap.throws(() => evalTokens(tokenize('2 + sin')));
+tap.throws(() => evalTokens(tokenize('(2+2')));
