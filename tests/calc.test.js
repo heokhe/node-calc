7eaa9b98804d97b8c0c323390bf8c3f3fe9e901a
Hosein Khansari <hrk.x11@gmail.com>
2020-06-09 23:02:02 +0430

    Improve tests

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,32 +1,43 @@
 const tap = require('tap');
 const { evalExpression } = require('../lib');
 
-const R = Math.PI / 180;
-
-// these are tests for basic calculations
-
+// basic calculations
 tap.equals(evalExpression(' 2 * 2'), 4);
 tap.equals(evalExpression('1 + 2 + 3 + 4'), 10);
 tap.equals(evalExpression('2 / 2'), 1);
+
+// parenthesis
 tap.equals(evalExpression('2(5 + 2)3'), 2 * 7 * 3);
 tap.equals(evalExpression('-(2-3)(2+3)'), 5);
-tap.equals(evalExpression('+(2-3)'), -1);
+tap.equals(evalExpression('+((4*4)-3)'), 13);
+
+// pow
 tap.equals(evalExpression('3 ^ 2 ^ 0'), 3);
 tap.equals(evalExpression('2 * 3 ^ 2'), 18);
-tap.equals(evalExpression('-2cos(180)'), 2);
-tap.equals(evalExpression('-cos((90*2))'), 1);
-tap.equals(evalExpression('2-cos(180)'), 3);
-tap.equals(evalExpression('sin(30)10'), 10 * Math.sin(30 * R));
-tap.equals(evalExpression('cot(30)'), 1 / Math.tan(30 * R));
-tap.equals(evalExpression('tan(30)'), Math.tan(30 * R));
-tap.equals(evalExpression('sqrt(4)'), 2);
-tap.equals(evalExpression('2cbrt(-8)'), -4);
-tap.equals(evalExpression('4!'), 4 * 3 * 2);
-tap.equals(evalExpression('0!'), 1);
-tap.equals(evalExpression('(2+3)!'), 5 * 4 * 3 * 2);
+
+// root functions
+tap.equals(evalExpression('cbrt(-8)'), -2);
+tap.equals(evalExpression('sqrt(4)(3)'), 6);
+tap.equals(evalExpression('sqrt(4)5'), 10);
+tap.equals(evalExpression('2sqrt(4)'), 4);
+
+// factorial
+tap.equals(evalExpression('7(2+3)!'), 7 * 5 * 4 * 3 * 2);
 tap.equals(evalExpression('2!3!'), 2 * 6);
-tap.equals(evalExpression('2!sqrt(4)'), 4);
-tap.equals(evalExpression('sqrt(4)(4)'), 8);
-tap.equals(evalExpression('sqrt(4)4'), 8);
+tap.equals(evalExpression('-4!'), -4 * 3 * 2);
+tap.equals(evalExpression('1-0!'), 0);
 tap.throws(() => evalExpression('2.5!'));
-tap.throws(() => evalExpression('fact(-1)'));
+tap.throws(() => evalExpression('(-1)!'));
+
+// trigonometric functions: sin
+tap.equals(evalExpression('sin(-30)'), 0.5);
+tap.equals(evalExpression('sin(170)'), evalExpression('sin(10)'));
+tap.equals(evalExpression('sin(270)'), -1);
+
+
+// trigonometric functions: cos
+tap.equals(evalExpression('cos(240)'), -0.5);
+tap.equals(evalExpression('cos(270)'), 0);
+
+// trigonometric functions: tan & cot
+tap.equals(evalExpression('cot(4)'), 1 / evalExpression('tan(4)'));
